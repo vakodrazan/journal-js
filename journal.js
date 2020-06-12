7ed9c8ac025b81f104e331cde44d0b51e6728b91
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-06-12 10:53:12 +0300

    Use an object array for the journal entries

--- journal.js
+++ journal.js
@@ -1,17 +1,18 @@
-class journalObj {
-    constructor(title, content) {
-        this.title = title;
-        this.content = content; 
+let JournalEntries = [
+    {
+        title: 'Description of the day', 
+        content: "It's been raining for whole week in here, but today it's changed a bit because the sun raise."
+    },
+
+    {
+        title: "Learning coding", 
+        content:'Learning coding is hard but it can alse be fun if you understand the structure of the thing you do'
+    },
+
+    {
+        title: "Go for a camping trip", 
+        content: 'When people want to go fo a camping trip, the first thing you need is tent you should always bring a tent.'
     }
-};
+]
 
-
-
-let describeTheDay = new journalObj('Description of the day', "It's been raining for whole week in here, but today it's changed a bit because the sun raise."
-);
-
-let learnCoding = new journalObj("Learning coding", 'Learning coding is hard but it can alse be fun if you understand the structure of the thing you do'
-);
-
-let campingTrip = new journalObj("Go for a camping trip", 'When people want to go fo a camping trip, the first thing you need is tent you should always bring a tent.'
-);
+console.log(JournalEntries)

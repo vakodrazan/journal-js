745cba450c19bacb3b8423da5ac96b7b31c9402e
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-06-12 08:52:51 +0300

    Create a class object

--- journal.js
+++ journal.js
@@ -1,1 +1,17 @@
-console.log('it works');
+class journalObj {
+    constructor(title, content) {
+        this.title = title;
+        this.content = content; 
+    }
+};
+
+
+
+let describeTheDay = new journalObj('Description of the day', "It's been raining for whole week in here, but today it's changed a bit because the sun raise."
+);
+
+let learnCoding = new journalObj("Learning coding", 'Learning coding is hard but it can alse be fun if you understand the structure of the thing you do'
+);
+
+let campingTrip = new journalObj("Go for a camping trip", 'When people want to go fo a camping trip, the first thing you need is tent you should always bring a tent.'
+);

cce933f23b642f1b09e9e013f1f4a7d6b4083d9c
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-06-13 07:55:10 +0300

    Restart from the beginning using while loop

--- journal.js
+++ journal.js
@@ -15,25 +15,33 @@ let journalEntries = [
     }
 ];
 
+let journalRequirement = '';
 
-let journalChoice = Number(prompt("Welcome to my journal! \n Choose (1) for listing all the entries. \n Choose (2) for adding new entry. \n choose (3) to quit."));
-
-if (journalChoice >= 1 && journalChoice <= 3) {
-
-    if (journalChoice === 1) {
-        for (let i = 0; i < JournalEntries.length; i++) {
-            alert(journalEntries);
-        }
-
-    } else if (journalChoice === 2) {
-        newJournalTitle = String(prompt("Enter journal title"))
-        if (newJournalTitle) {
-            newJournalContent = String(prompt("Enter you new chosen content relate to your title: "));
-            alert(`Title: ${newJournalTitle} \n Content: ${newJournalContent}`)
-        }
-        journalChoice = Number(prompt("Welcome to my journal! \n Choose (1) for listing all the entries. \n Choose (2) for adding new entry. \n choose (3) to quit."));
+while (journalRequirement === '') {
+    let journalChoice = Number(prompt("Welcome to my journal! \n Choose (1) for listing all the entries. \n Choose (2) for adding new entry. \n choose (3) to quit."));
+    while (journalChoice < 0 || journalChoice > 3) {
+        journalChoice = Number(prompt("Must be number between 1 and 3! \n Choose (1) for listing all the entries. \n Choose (2) for adding new entry. \n choose (3) to quit."));
     }
-
-    journalChoice = Number(prompt("Welcome to my journal! \n Choose (1) for listing all the entries. \n Choose (2) for adding new entry. \n choose (3) to quit."));
-
+    
 }
+
+
+
+
+// if () {
+
+//     if (journalChoice === 1) {
+//         }
+
+//     } else if (journalChoice === 2) {
+//         newJournalTitle = String(prompt("Enter journal title"))
+//         if (newJournalTitle) {
+//             newJournalContent = String(prompt("Enter you new chosen content relate to your title: "));
+//             alert(`Title: ${newJournalTitle} \n Content: ${newJournalContent}`)
+//         }
+//         journalChoice = Number(prompt("Welcome to my journal! \n Choose (1) for listing all the entries. \n Choose (2) for adding new entry. \n choose (3) to quit."));
+//     }
+
+//     journalChoice = Number(prompt("Welcome to my journal! \n Choose (1) for listing all the entries. \n Choose (2) for adding new entry. \n choose (3) to quit."));
+
+// }

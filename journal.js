0158d58e686695f09f5763e90d3fad2310c6fe01
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-06-16 16:18:14 +0300

    Create functions for some conditions and call it inside the loop

--- journal.js
+++ journal.js
@@ -17,6 +17,34 @@ let journalEntries = [
     }
 ];
 
+const entryList = () => { // Function for showing all the list
+    for (let i = 0; i < journalEntries.length; i++) {
+        alert(`Title: ${journalEntries[i].title} \n Content: ${journalEntries[i].content}`);
+    }
+};
+
+const addEntry = () => { // Function to ask the user their new entry
+    let title = prompt('Enter your new journal entry title');
+    let content = prompt('Enter your new chosen journal entry content: ');
+
+    const newEntry = {
+        title: title, 
+        content: content
+    };
+
+    journalEntries.push(newEntry);
+    for (let i = 0; i <journalEntries.length; i ++) {
+        alert(`Title: ${journalEntries[i].title} \n Content: ${journalEntries[i].content}`);
+    }
+};
+
+const deleteIndex = () => { // Function when delete a new specific entry
+    let indexToDelete = Number(prompt(`Enter the entry's number that you want to delete (1 to ${journalEntries.length})`));
+    let deleteEntry = journalEntries.splice(indexToDelete--, 1);
+    alert(`You have deleted this: ${deleteEntry[0].title}`)
+
+}
+
 let journalRequirement = '';
 
 while (journalRequirement === '') { // Ask user's choice
@@ -37,23 +65,10 @@ Choose (5) to delete a specific entry. (with its index)
     }
 
     if (journalChoice === 1) { // Checking all the entry list
-        for (let i = 0; i < journalEntries.length; i++) {
-            alert(`Title: ${journalEntries[i].title} \n Content: ${journalEntries[i].content}`);
-        }
-
+        entryList();
     } else if (journalChoice === 2) { // Adding a new entry
-        let title = prompt('Enter journal title');
-        let content = prompt('Enter you new chosen content relate to your title: ');
 
-        const newEntry = {
-            title: title, 
-            content: content
-        };
-
-        journalEntries.push(newEntry);
-        for (let i = 0; i <journalEntries.length; i ++) {
-            alert(`Title: ${journalEntries[i].title} \n Content: ${journalEntries[i].content}`);
-        }
+        addEntry();
 
     } else if (journalChoice === 3) { // Quite the program
         alert("Thank you for taking parts in my journal entries");
@@ -63,10 +78,9 @@ Choose (5) to delete a specific entry. (with its index)
         let entryDeleted = journalEntries.pop();
         alert(`You have deleted the last entry: ${entryDeleted.title}`);
 
-    } else if (journalChoice === 5) { // Delete a specific array
-        let indexToDelete = Number(prompt(`Enter the entry's number that you want to delete (1 to ${journalEntries.length})`));
-        let deleteEntry = journalEntries.splice(indexToDelete--, 1);
-        alert(`You have deleted this: ${deleteEntry[0].title}`)
+    } else if (journalChoice === 5) { // Delete a specific entry
+
+        deleteIndex();
 
     }
 }
